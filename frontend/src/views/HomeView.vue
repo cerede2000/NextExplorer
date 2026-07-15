274a64db1b2bf369bef866f6d003604954bb82f3
Benjy <benjy@MacBook-Pro-de-Benjy.local>
2026-07-15 13:55:02 +0200

    Compact home volume cards

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -123,7 +123,7 @@ const openPersonal = () => {
           :key="vol.name"
           type="button"
           @click="openItem(vol)"
-          class="grid w-fit max-w-full grid-cols-[4rem_minmax(0,11rem)] items-start gap-x-3 rounded-lg px-2 py-3 text-left transition-colors hover:bg-neutral-100/70 dark:hover:bg-neutral-800/60"
+          class="grid w-fit max-w-full grid-cols-[4rem_minmax(0,9rem)] items-start gap-x-3 rounded-lg px-2 py-3 text-left transition-colors hover:bg-neutral-100/70 dark:hover:bg-neutral-800/60"
         >
           <IconDrive class="h-16 shrink-0" />
           <div class="flex w-full min-w-0 flex-col items-stretch gap-2 pt-1">
